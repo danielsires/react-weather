7af441c505e1abceb4537f1dd350e71477186ac3
Daniel Sires <dsires13@mac.com>
2023-05-11 22:04:45 -0400

    fixed five day and icons

--- src/components/FiveDay.jsx
+++ src/components/FiveDay.jsx
@@ -6,14 +6,17 @@ function FiveDay({ forecast }) {
   const filteredData = data
     ? data.filter((obj) => obj.dt_txt.slice(11, 13) === '12')
     : null;
-
+  console.log(filteredData);
   if (filteredData) {
     return filteredData.map((obj, key) => (
       <div id="day" key={key}>
         <p>{dayjs(obj.dt_txt.split(' ')[0]).format('MM/DD/YYYY')}</p>
         <p>{dayjs(obj.dt_txt.split(' ')[0]).format('dddd')} </p>
-        <p>Temp High: {Math.ceil(obj.main.temp_max)}</p>
-        <p>Temp Low: {Math.ceil(obj.main.temp_min)}</p>
+        <p>Temp: {Math.ceil(obj.main.temp)}</p>
+        <img
+          src={`http://openweathermap.org/img/w/${obj.weather[0].icon}.png`}
+          alt="weather icon"
+        />
       </div>
     ));
   } else {
